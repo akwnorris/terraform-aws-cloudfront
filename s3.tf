5be2eaed476de45196ec160d5abf3f3b303a46bd
Alex Norris <akwnorris@gmail.com>
2024-07-09 10:09:30 -0500

    block public access configuration to S3 bucket

--- s3.tf
+++ s3.tf
@@ -4,4 +4,10 @@ provider "aws" {
 
 resource "aws_s3_bucket" "this" {
   bucket = "static-"
+
+  # Block public access
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
