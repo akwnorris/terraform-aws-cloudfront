c0e75100ba1d61e88f35118adacaeee39fc2fe0a
Alex Norris <akwnorris@gmail.com>
2024-07-09 10:10:14 -0500

    add index.html file to S3 bucket

--- s3.tf
+++ s3.tf
@@ -11,3 +11,10 @@ resource "aws_s3_bucket" "this" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_object" "this" {
+  bucket       = aws_s3_bucket.this.bucket
+  key          = "index.html"
+  source       = "index.html"
+  content_type = "text/html"
+}
